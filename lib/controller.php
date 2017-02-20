1d2178b7f7c35bd2e190f6f4f0adf555bddaeddf
Roderic Linguri <linguri@digices.com>
2017-02-20 13:59:58 -0700

    Update controller.php

--- lib/controller.php
+++ lib/controller.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * @package OTMVC
- * @version 0.0.1
- * @date    2017-02-19
- * @author  Roderic Linguri <linguri@digices.com>
- * @license MIT
+ * @package   OTMVC
+ * @version   0.0.1
+ * @copyright 2017-02-19
+ * @author    Roderic Linguri <linguri@digices.com>
+ * @license   MIT
  */
 
 abstract class OTMVCController
